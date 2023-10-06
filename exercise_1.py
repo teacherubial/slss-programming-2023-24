2457e998518216279330cff32c79893e0060f171
Tim Ubial <42955945+teacherubial@users.noreply.github.com>
2023-10-06 09:37:46 -0700

    added exercise

---
+++ exercise_1.py
@@ -0,0 +1,14 @@
+# Exercise 1
+# Author: Ubial
+# 6 October 2023
+
+# Find the problem
+
+korean_food = ["bibimbap", "Budae jiggae", "kimbap"]
+
+fave_food = input("What's your favourite food? ")
+
+if fave_food.lower().strip("!,.?") in korean_food:
+    print("I think you like Korean food.")
+else:
+    print("Sorry I can't help you.")
