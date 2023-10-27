ba38d3946cff72226b0b46098d5240f009c5f107
Tim Ubial <tubial@sd38.bc.ca>
2023-10-27 09:08:42 -0700

    Number practice

---
+++ number_practice.py
@@ -0,0 +1,16 @@
+# Number Practice
+# Author: Ubial
+# Date: 26 October 2023
+
+# Operators that work on Numbers
+print(1 + 1)
+print(1 - 1)
+print(10 * 10)    # Multiply
+print(9 / 3)      # Division
+
+print(1 + 2 / 4 + 5 - 4)  # BEDMAS
+
+print(9 // 2)     # Floor division
+print(9 %  2)     # Modulo gives remainder
+
+print(2 ** 10)    # Power 
