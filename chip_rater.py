3828dd6bb073afe70bfd741cb0a6fd362f53e446
Tim Ubial <tubial@sd38.bc.ca>
2023-11-01 14:07:10 -0700

    added more to chip rater

--- chip_rater.py
+++ chip_rater.py
@@ -5,4 +5,19 @@
 # Interview people about their
 # perception of how delicious chips are
 # based on a set of questions
+# In the end, we'll give an aggregate score.
 
+# Greet the user
+print("Please answer the following questions based on the chip that you just ate.")
+
+# Create a list of questions to ask
+questions = [
+    ""
+]
+
+# Ask the questions to the user
+    # Store their response
+
+# Do some math
+
+# Present the results to the user
