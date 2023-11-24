b43f3ce82f8c90a67138805d0291c602a05b8fee
Tim Ubial <tubial@sd38.bc.ca>
2023-11-24 14:22:09 -0800

    functions!

--- functions_practice.py
+++ functions_practice.py
@@ -3,7 +3,21 @@
 # 24 November 2023
 
 
-def area_of_a_square(sidelength: float):
+def area_of_a_square(sidelength: float) -> float:
+    """Return the area of a square.
+    Results are in units squared.
+
+    Params:
+
+    sidelength - length of one side of the square
+    """
+
+    area = sidelength**2
+
+    return area
+
+
+def print_area_of_a_square(sidelength: float) -> None:
     """Calculate and print the area of a square.
     Results are in units squared.
 
@@ -17,3 +31,14 @@ def area_of_a_square(sidelength: float):
     print(
         f"The area of a square with side length {sidelength} is: {area} square units."
     )
+
+
+print(print_area_of_a_square(12.2))
+# print_area_of_a_square(12)
+
+# Given two squares of two sidelengths
+#    12.2 and 144
+# Add the area of both squares
+
+# area_of_squares = area_of_a_square(12.2) + area_of_a_square(144)
+# print(area_of_squares)
