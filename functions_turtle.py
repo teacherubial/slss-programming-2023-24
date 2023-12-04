eb467869b824dc47d6e5a62f1f7a1a6c56a1d643
Tim Ubial <tubial@sd38.bc.ca>
2023-12-04 14:28:56 -0800

    trees!

--- functions_turtle.py
+++ functions_turtle.py
@@ -27,10 +27,46 @@ def draw_square(side_length: int) -> None:
     burt.left(90)
 
 
-burt.speed(0)
+def draw_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree
+    with initial given height in pixels
 
-# Draw squares that grow exponentially
-for i in range(40):
-    draw_square(squared(i))
+    Params:
+
+    level - number representing the levels of branches
+    height - height of the main trunk in pixels
+    """
+
+    if level > 0:
+        # 1. Move turtle forward height pixels
+        burt.forward(height)
+
+        # 2. Turn turtle left
+        burt.left(36)
+        #    a. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height * 0.75)
+
+        # 3. Turn turtle right
+        burt.right(36 * 2)
+        #    a. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height * 0.75)
+
+        # 4. Return to beginning
+        burt.left(36)
+        burt.back(height)
+    else:
+        original_colour = burt.color()
+        burt.color("green")
+        burt.stamp()
+        burt.color(original_colour[0])  # revert burt's colour
+
+
+# Set-up Burt to draw trees
+burt.color("brown")
+burt.setheading(90)  # points burt north
+burt.width(4)  # trunk and branches thicker
+burt.speed(5)
+
+draw_tree(10, 175)
 
 turtle.done()
