c0d97b268c990fb733d65a442fc8a4f8e5a089de
Tim Ubial <tubial@sd38.bc.ca>
2023-12-13 11:15:36 -0800

    PIXELSSSSS!

--- images_example.py
+++ images_example.py
@@ -4,13 +4,38 @@
 
 from PIL import Image
 
+
+def pixel_to_string(pixel: tuple) -> str:
+    """Take a rgb 3-tuple and "interpret it"
+    as a colour and return that colour's name
+
+    Params:
+        pixel - 3-tuple of (red, green, blue)
+
+    Return:
+        String representing the colour
+    """
+    r, g, b = pixel
+
+    if g > 250 and r < 32 and b < 32:
+        return "green"
+
+
 # Recall that we can open up files in Python
 with Image.open("./Images/kid-green.jpg") as im:
-    # get the pixel information from the top left most pixel
-    pixel = im.getpixel((0, 0))
+    # algorithm to visit every pixel in the kid-green image
+    # store the height and width of the image
+    image_height = im.height
+    image_width = im.width
 
-    # print the pixel information
-    print(pixel)
+    # outer loop is top->bottom
+    # inner loop is left->right
+    for y in range(image_height):
+        for x in range(image_width):
+            pixel = im.getpixel((x, y))
 
-    # get the middle pixel
-    middle = im.width
+            # Detects green pixels
+            if pixel_to_string(pixel) == "green":
+                print("GREEN PIXEL!!!!")
+            else:
+                print("UNKNOWN PIXEL")
